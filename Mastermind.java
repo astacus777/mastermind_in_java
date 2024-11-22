c1461cb42626e12ec9f491770ecb63a3c597ffb2
astacus777 <astacus@hotmail.co.uk>
2024-11-22 19:46:54 +0100

    Apply final adjustments and code cleanup

--- Mastermind.java
+++ Mastermind.java
@@ -2,29 +2,36 @@ import java.util.Random;
 
 // Main class to play the game
 public class Mastermind {
-        String secretNumber = getRandomNumber();
-        boolean isEquel = false;
+    private String secretNumber = getRandomNumber();
+    private boolean isEquel = false;
 
         // Method to start the game
         public void startGame() {
 
         // Uncomment to display the secret number for debugging
-        // System.out.println("Random number (for debugging): " + secretNumber);
+        System.out.println("Random number (for debugging): " + secretNumber);
+        int steps = 0;
 
         while (!isEquel) {
             String usersGuess = UserInput.getNumber();
             int clue1 = 0;
             int clue2 = 0;
+            
             try {
                 // Check if the user's guess matches the secret number
+                
                 int userNumber = Integer.parseInt(usersGuess);
                 if (userNumber == Integer.parseInt(secretNumber)) {
                     isEquel = true;
+                    steps ++;
                     System.out.println("Congratulations! You guessed the number: " + secretNumber);
+                    System.out.println("Your score is: " + steps);
                 } else {
                     // Provide clues
-                    clue1 = correcPlacedtNumbers(usersGuess, secretNumber);
+                    clue1 = correctPlacedNumbers(usersGuess, secretNumber);
                     clue2 = correctNumbers(usersGuess, secretNumber);
+                    steps++;
+                    System.out.println("This is your " + steps + " attempt");
                     System.out.println("Unfortunately, this is a wrong guess! Please try again.");
                     System.out.println("Here are some clues: ");
                     System.out.println(" - Correct digits in correct places: " + clue1);
@@ -46,7 +53,7 @@ public class Mastermind {
     }
     
     // Function to check the number of correct digits in the correct positions
-    private static int correcPlacedtNumbers(String user, String secret) {
+    private static int correctPlacedNumbers(String user, String secret) {
         int number = 0;
         for (int i = 0; i< user.length(); i++) {
             if (user.charAt(i) == secret.charAt(i)) {
@@ -59,7 +66,7 @@ public class Mastermind {
     // Function to check the total number of guessed digits, regardless of position
     private static int correctNumbers(String user, String secret) {
         int number = 0;
-        boolean[] arr = new boolean[user.length()];;
+        boolean[] arr = new boolean[user.length()];
 
         for (int i=0; i<user.length(); i++){
             for (int j=0; j<secret.length(); j++ ) {
--- UserInput.java
+++ UserInput.java
@@ -1,41 +1,51 @@
+// Scanner library attached
 import java.util.Scanner;
 
 public class UserInput {
+    // A new instance of Scanner
     private static final Scanner scanner = new Scanner(System.in);
     public static String getNumber() {
         boolean isValid = false; 
         String guess = "";
-        int steps = 0;
-        while (!isValid) {
-            System.out.println("Please give me your best guess(just remember it has to be a 4-digit number!): ");
-            guess = scanner.nextLine().trim();
 
-            if (guess.matches("\\d{4}")) {
-                steps++;
-                System.out.println("This is your " + steps + " attempt"); 
-                isValid = true;
-            } else {
-                System.out.println("I'm sorry but this is an Invalid input. Please Try again.");  
-                steps++;
-                System.out.println("This is your " + steps + " attempt");     
+        // Checks if the user input is meeting criteria
+        while (!isValid) {
+            try {
+                System.out.println("Please give me your best guess(just remember it has to be a 4-digit number!): ");
+                guess = scanner.nextLine().trim();
+    
+                if (guess.matches("\\d{4}")) {
+                    isValid = true;
+                } else {
+                    System.out.println("I'm sorry but this is an Invalid input. Please enter exactly a 4-digit number.");
+                }
+            } catch (Exception e) {
+                System.out.println("An error occurred while reading your input. Please try again.");
             }
+           
         }
         return guess;
     }
 
+    // Checks whether the player would like to continue the game or not
     public static boolean playAgain() {
-            while (true) { // Pętla do obsługi odpowiedzi użytkownika
-                System.out.println("Would you like to play one more time? (type 'yes' to coninue or any key to finish)");
-                String answer = scanner.nextLine().trim();
-    
-                if (answer.equalsIgnoreCase("yes")) {
-                    return true;
-                } else {
-                    return false;
+            while (true) { 
+                try {
+                    System.out.println("Would you like to play one more time? (type 'yes' to continue or any key to finish)");
+                    String answer = scanner.nextLine().trim();
+        
+                    if (answer.equalsIgnoreCase("yes")) {
+                        return true;
+                    } else {
+                        return false;
+                    } 
+                }  catch (Exception e) {
+                    System.out.println("An error occurred while reading your input. Please try again.");
                 }
             }
     }
 
+    // Closes the scanner
     public static void closeScanner() {
         scanner.close();
     }
